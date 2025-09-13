331a9afb5e8382097b5f4c8d3dc4aa5e43dfc5ed
Yurex <yusufekungomi@gmail.com>
2025-09-13 07:36:01 +0100

    tailwind issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,6 @@ export default function Home() {
     <div className='text-center px-4 space-y-4'>
       <h1 className='text-4xl font-bold'>Welcome to the homepage</h1>
       <p>
-        {/* The more you observe, the more you understand, it comes with a side
-        effect, little thing start that you don't like to irritate you and it's
-        not nuture, it's nature, your body has already adapted to the things you
-        love */}
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
         ullam quaerat quibusdam fuga ut odit molestias officia ratione quod
         incidunt at quae consectetur hic in alias, ex facilis possimus
