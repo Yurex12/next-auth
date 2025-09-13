3df8bec3919918b00a4bfb2a231dd9b8d2331f69
Yurex <yusufekungomi@gmail.com>
2025-09-13 09:04:19 +0100

    fixing an error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 export { auth as middleware } from '@/auth';
 
+// export const config = {
+//   matcher: [
+//     '/((?!api/auth|auth/signup|auth/signin|_next/static|_next/image|favicon.ico).*)',
+//   ],
+// };
+
 export const config = {
-  matcher: [
-    '/((?!api/auth|auth/signup|auth/signin|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/dashboard'],
 };
